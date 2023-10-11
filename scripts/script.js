93a2922940a0b09529110cdddf88ce2a24aa50e7
AlexandYOr <avo01121997@yandex.ru>
2023-10-11 16:33:39 +0300

    create move functional | lesson22hard

--- scripts/script.js
+++ scripts/script.js
@@ -10,28 +10,23 @@ let blocks = document.querySelectorAll('.block')
 blocksBody.addEventListener('click', (e) => {
     blocks.forEach((elem, index) => {
         if (e.target.closest('.block') === elem) {
-            if (e.target.closest('.left')) {
-                const num = e.target.closest('.block').querySelector('.block-number').textContent
+            const num = e.target.closest('.block').querySelector('.block-number').textContent
+            if (e.target.closest('.left') && index > 0) {
                 blocks[index].querySelector('.block-number').textContent = blocks[index - 1].querySelector('.block-number').textContent
                 blocks[index - 1].querySelector('.block-number').textContent = num
             }
-            if (e.target.closest('.right')) {
-                const num = e.target.closest('.block').querySelector('.block-number').textContent
+            if (e.target.closest('.right') && index < 24 ) {
                 blocks[index].querySelector('.block-number').textContent = blocks[index + 1].querySelector('.block-number').textContent
                 blocks[index + 1].querySelector('.block-number').textContent = num
             }
-            if (e.target.closest('.top')) {
-                const num = e.target.closest('.block').querySelector('.block-number').textContent
+            if (e.target.closest('.top') && index > 4) {
                 blocks[index].querySelector('.block-number').textContent = blocks[index - 5].querySelector('.block-number').textContent
                 blocks[index - 5].querySelector('.block-number').textContent = num
             }
-            if (e.target.closest('.bottom')) {
-                const num = e.target.closest('.block').querySelector('.block-number').textContent
+            if (e.target.closest('.bottom') && index <= 19) {
                 blocks[index].querySelector('.block-number').textContent = blocks[index + 5].querySelector('.block-number').textContent
                 blocks[index + 5].querySelector('.block-number').textContent = num
             }
         }
     })
-    console.log(e.target.closest('.block'))
-    return blocks
 })
